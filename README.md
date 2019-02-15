06887e444822658858574be081363556a5a5cad1
saeed79darvish <s79darvish@gmail.com>
2019-02-15 08:53:40 -0800

    SaeedToDoList

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 Create Read Update and Delete framework using JS
 
 
-## Tasks
 
-### Basic Reqs
-- [ ] Where to store data? (localstorage)
-- [ ] How to caputure data? (web form)
-- [ ] How to modify data? (update action, delete action)
-- [ ] How to view data? (style?)
-- [ ] UI/UX considerations (how are we going to use this)
+      In this project I used bootstrap and fontawesome to give a good style to my application
+      I made a div that is a container for whole body and I put a form tag in the container
+      to get input of users and we have two input title and item, below the form I add a button to
+      Add item to the list and finally I made a table to sort data an display our data in the Table
 
-### Advanced Reqs
-- [ ] Item that I added goes here...
+      after that I define a app.js file and in this file I used define three classes
+      Lists to represents a list
+      UI  to Handle UI tasks
+      Store Handle storage with localStorage
--- app/app.js
+++ app/app.js
@@ -1,48 +1,127 @@
-/*
-Init app
-interact with DOM
-interact with localstorage
+//List Class represents a List
+class List {
+  constructor(title,item) {
 
- */
+    this.title=title;
+    this.item=item;
+ }
+}
 
-$(document).ready(function(){
-  // this is where we jquery
-  //var keyData = 'ourKey'; // going to need to make this dynamic?
+//UI Class Handle UITasks
+class UI{
 
-
-  $('.btn-add').on('click', function(e){
-    console.log(e);
-    var keyData = $('.input-key').val();
-    var valueData = $('.input-value').val();
-    // write to db
-    localStorage.setItem(keyData, valueData);
-    // read from db
-    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
-    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-    $('.input-key').val('');
-    $('.input-value').val('');
+static displayLists(){
+    const lists = Store.getLists();
+    lists.forEach(function(item){
+    UI.addItemToList(item)
   });
+}
+
+static showAlert(message,className){
+  const div= document.createElement('div');
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#list-form');
+  container.insertBefore(div,form);
+  setTimeout(function(){
+    document.querySelector('.alert').remove()
+  },2000);
+}
+
+static addItemToList(item){
+    const list = document.querySelector('#item-list')
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${item.title}</td>
+    <td>${item.item}</td>
+
+    <!-- <td>{book.isbn}</td> -->
+    <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
+    `;
+    list.appendChild(row);
+}
 
 
-  // update db
-    // need to expand when  more than 1 item is added
+ static deleteItem(elem){
+   if(elem.classList.contains('delete')) {
+     elem.parentElement.parentElement.remove();
+   }
+ }
 
-  // delete item
-  $('.container-data').on('click', '.display-data-item', function(e){
-    console.log(e.currentTarget.dataset.keyvalue);
-    var keyData = e.currentTarget.dataset.keyvalue;
-    localStorage.removeItem(keyData);
-    $('.container-data').text('');
-  });
-  // delete all?
-  $('.btn-clear').click(function(){
-    localStorage.clear();
-    $('.container-data').text('');
-  });
 
+ static clearFileds(){
+   document.querySelector('#title').value="";
+   document.querySelector('#item').value="";
+   }
+}
+
+//Store Class Handle Storege
+class Store {
+
+
+  static getLists(){
+     let lists;
+     if(localStorage.getItem('lists') === null){
+      lists=[];
+     }else{
+        lists=JSON.parse(localStorage.getItem('lists'));
+     }
+    return lists;
+  }
+
+
+static addList(list){
+    const lists = Store.getLists();
+    lists.push(list);
+    localStorage.setItem('lists',JSON.stringify(lists));
+  }
+
+
+  static removeList(item){
+     const lists = Store.getLists();
+     lists.forEach((list,index) => {
+       if(list.item === item){
+         lists.splice(index,1);
+       }
+     });
+     localStorage.setItem('lists',JSON.stringify(lists));
+  }
+}
+
+
+//Event Display Lists
+document.addEventListener('DOMContentLoaded', UI.displayLists);
+//Event Add a item
+document.querySelector('#list-form').addEventListener('submit',function(e){
+
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const item = document.querySelector('#item').value;
+
+  //Use an if statment to validate input
+  if(title === '' || item === '' ){
+    UI.showAlert('please insert something','danger')
+  }else{
+    const list = new List(title,item);
+    UI.addItemToList(list);
+    Store.addList(list);
+    UI.clearFileds();
+    UI.showAlert('item added','success')
+
+  }
+});
+
+
+
+//Event remove a item from list
+document.querySelector('#item-list').addEventListener('click',function(e) {
+  //Remove item from UI
+  UI.deleteItem(e.target);
+  // Remove item from localStorage
+  Store.removeList(e.target.parentElement.previousElementSibling.textContent);
+  //Show a success alert when you remove item
+  UI.showAlert('item deleted','success')
 });
